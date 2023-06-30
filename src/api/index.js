aebecfda9ad7d7400e2ac7565c54482fc71c79c0
volodymyr1v <volodumur.verhyn@gmail.com>
2023-06-30 14:18:28 +0300

    api function

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
+// звідси стартуєм усю роботу з бекендом
 import axios from 'axios';
 
-// звідси стартуєм усю роботу з бекендом
+
+// функція отримує посилання на категорії
+// функція повертає обєкт
 export const getCategories = () => {
     return axios.get('https://books-backend.p.goit.global/books/category-list')
   .then(function (response) {
@@ -14,6 +17,26 @@ export const getCategories = () => {
   
 };
 
+// функція отримує посилання на популярні книги
+// функція повертає обєкт
+export const getBooksAllCategories = () => {
+    return axios.get('https://books-backend.p.goit.global/books/top-books')
+  .then(function (response) {
+    
+    return response;
+  })
+  .catch(function (error) {
+    
+    console.log(error);
+  })
+  
+};
+
+
+
+// функція отримує посилання на книги окремої категорії
+// у аргументи функції додати обрану категорію
+// функція повертає обєкт
 export const getBooksByCategory = (category) => {
     return axios.get(`https://books-backend.p.goit.global/books/category?category=${category}`)
   .then(function (response) {
@@ -28,6 +51,9 @@ export const getBooksByCategory = (category) => {
 };
 
 
+// функція отримує посилання на книгу
+// у аргументи функції додати ID обраної книги
+// функція повертає обєкт
 export const getBookById = (bookId) => {
     return axios.get(`https://books-backend.p.goit.global/books/${bookId}`)
   .then(function (response) {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 
-import {getCategories} from '../src/api';
+/////////////////////////////////////////тест коду api
 
+// import {getCategories} from '../src/api';
+// import { getBooksAllCategories} from '../src/api'
 
-(async function (){
-    const response = await getCategories();
-    console.log(response);
-})();
+// (async function (){
+//     const response = await getCategories();
+//     console.log(response);
+// })();
 
+// (async function (){
+//     const response = await getBooksAllCategories();
+//     console.log(response);
+// })();
+/////////////////////////////////////////////////
