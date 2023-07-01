3b8a7de437c5873b996a8a88a3b8933cffaed741
kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-07-01 07:56:04 +0000

    Deploying to gh-pages from @ kornieiev/CodeBusters-project@966414c0d0dd8a8633b4f5a3b73dae940c378d7d 🚀

---
+++ index.0ae9cf3b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".menu-link"),n=document.querySelector(".dropdown-menu");e.addEventListener("click",function(e){e.preventDefault(),n.classList.toggle("visually-hidden")})}();
+//# sourceMappingURL=index.0ae9cf3b.js.map
---
+++ index.42f6345f.js
@@ -0,0 +1,2 @@
+let e=document.querySelector(".menu-link"),n=document.querySelector(".dropdown-menu");e.addEventListener("click",function(e){e.preventDefault(),n.classList.toggle("visually-hidden")});
+//# sourceMappingURL=index.42f6345f.js.map
